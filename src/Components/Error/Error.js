423e8a1295b780146e6f1a08e9828e0f8998cb71
Abdullayev2005 <108843760+Abdullayev2005@users.noreply.github.com>
2023-02-20 22:54:14 +0500

    finish

---
+++ src/Components/Error/Error.js
@@ -0,0 +1,14 @@
+
+const Error = () => {
+    return (
+        <div>
+            <div className="max-w-xl bg-red-500 p-5 mx-5 text-white ">
+                <h2 className="text-2xl">Please, Type Valid Pokemon Name</h2>
+                <br />
+                <p className="text-xl">For Example: charmander, squirtle, blastoise, bulbasaur, ivysaur </p>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
