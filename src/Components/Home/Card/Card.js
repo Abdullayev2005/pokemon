0a46b9dde97b4537d2ea38fa714110d7f851da80
Abdullayev2005 <108843760+Abdullayev2005@users.noreply.github.com>
2023-02-20 23:17:29 +0500

    dsa

--- src/Components/Home/Card/Card.js
+++ src/Components/Home/Card/Card.js
@@ -25,25 +25,25 @@ const Card = ({pokemon}) => {
         </div>
 
     </div>
-        // <div className="flex flex-wrap justify-center">
-        //     <div className="flex flex-col justify-center bg-black text-white rounded-lg shadow-md lg:w-3/12  m-6 px-5 overflow-hidden sm:w-full">
-        //         <h2 className="pl-10 pt-5  text-xl font-bold">{pokemon.name}</h2>
-        //         <div className="flex justify-center">
-        //             <img src={pokemon.img} alt="" className="w-6/12 " />
-        //         </div>
-        //         <div className="pl-10 pb-10">
-        //             <h4>Species Name: {pokemon.species}</h4>
-        //             <h4>Stats Name: {pokemon.hp}</h4>
-        //             <h4>Ability: {pokemon.ability}</h4>
-        //             <h4>WEight: {pokemon.weight}</h4>
-        //             <h4>Base-Experience: {pokemon.base_experience}</h4>
-        //             <h4>Version Name: {pokemon.game}</h4>
-        //         </div>
+        <div className="flex flex-wrap justify-center">
+           <div className="flex flex-col justify-center bg-black text-white rounded-lg shadow-md lg:w-3/12  m-6 px-5 overflow-hidden sm:w-full">
+                <h2 className="pl-10 pt-5  text-xl font-bold">{pokemon.name}</h2>
+                 <div className="flex justify-center">
+                    <img src={pokemon.img} alt="" className="w-6/12 " />
+                </div>
+                 <div className="pl-10 pb-10">
+                     <h4>Species Name: {pokemon.species}</h4>
+                     <h4>Stats Name: {pokemon.hp}</h4>
+                     <h4>Ability: {pokemon.ability}</h4>
+                     <h4>WEight: {pokemon.weight}</h4>
+                     <h4>Base-Experience: {pokemon.base_experience}</h4>
+                     <h4>Version Name: {pokemon.game}</h4>
+                 </div>
 
 
-        //     </div>
+             </div>
 
-        // </div>
+         </div>
     );
 };
 
